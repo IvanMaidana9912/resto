d5d3c693ee82acd9b0eb091175f577b420d9ccec
Ivan Maidana <imaidana@DarkJonux.local>
2023-06-08 23:43:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  // const [count, setCount] = useState(0)
+  return (
+      <div>
+        <h1>Hola Debi!! ahora ya si está para arrancar el proyecto</h1>
+      </div>
+  )
+}
