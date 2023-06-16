5ea0ca6cfa675c4ccc4b973079c4ed70d9e3a78a
Ivan Maidana <imaidana@DarkJonux.local>
2023-06-16 15:43:04 -0300

    first commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,10 +5,11 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import CardOverflow from '@mui/joy/CardOverflow';
 import Typography from '@mui/joy/Typography';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const minimoPrecioProducto = 0;
 
 export const Products = (product) => {
-  //console.log(`${id}, ${title}, ${price}, ${categoria}, ${images}`);
   const [[t, datas]] = Object.entries(product);
 
   return (
@@ -27,15 +28,15 @@ export const Products = (product) => {
         <Typography fontWeight="xl">{datas.title}</Typography>
 
         <Typography fontSize="xl" fontWeight="xl" sx={{ mt: 1 }}>
-          {datas.price > 0 ? `$${datas.price} Pesos` : "Sin Stock"}
+          {datas.price > minimoPrecioProducto ? `$${datas.price} Pesos` : "Sin Stock"}
         </Typography>
         <Typography level="body2">
-          <Link onClick={() => { window.open("https://www.facebook.com/", '_blank')}} className="text-decoration-none text-muted">Más Info</Link>
+          <Link onClick={() => { window.open("https://www.facebook.com/", '_blank') }} className="text-decoration-none text-muted">Más Info</Link>
         </Typography>
       </CardContent>
       <CardOverflow>
-        <Button variant="solid" size="lg" className='buttonCardProduct'>
-        Add to cart
+        <Button variant="solid" size="lg" className='buttonCardProduct' type='button'>
+          Add to cart
         </Button>
       </CardOverflow>
     </Card>
