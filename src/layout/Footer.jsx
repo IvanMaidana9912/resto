3d1cd70f35ff002b3b29f4f2c741b3dd2b231451
Ivan Maidana <imaidana@DarkJonux.local>
2023-06-16 23:54:57 -0300

    Updates

--- src/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <div className="imgLogo p-5 w-100 mt-5">
-            <p className="text-light">
-                hola este es el footer
-            </p>
-        </div>
-    )
-}
